bbe611bb531cf6b3aba6bfb34bb5a1f144cb4acf
EchoFUN <xukai.ken@gmail.com>
2013-06-05 19:21:28 +0800

    Push the code to the Github.

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+#!/user/bin/env node
+
+var fs = require('fs');
+
+var TARGET_FOLDER;
+
+process.argv.forEach(function(argv) {
+    if (argv.indexOf('--') <= -1)
+        return;
+            
+    switch(argv) {
+        case '--target':
+            TARGET_FOLDER = argv;
+    }
+});
+
+if (!TARGET_FOLDER) {
+    return;
+}
 
